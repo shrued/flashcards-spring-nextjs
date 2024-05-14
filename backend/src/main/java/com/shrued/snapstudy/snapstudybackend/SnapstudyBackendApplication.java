28f1d5627c7dfc8ca573e6d2954b20ed4efa6f04
shruedd <shrutisara10@gmail.com>
2024-05-13 23:36:20 -0400

    Project stack set up

---
+++ backend/src/main/java/com/shrued/snapstudy/snapstudybackend/SnapstudyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shrued.snapstudy.snapstudybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapstudyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapstudyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shrued/snapstudy/snapstudybackend/SnapstudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrued.snapstudy.snapstudybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapstudyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
